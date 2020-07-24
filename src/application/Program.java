7c0d4528c8c0a13cb438c02a3feb90b0e9e312dd
Luccas Yuri dos Santos <luccas.yuri@hotmail.com>
2020-07-23 22:44:16 -0300

    create first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Position pos = new Position(5, 10);
+		
+		System.out.println(pos);
 
 	}
 
